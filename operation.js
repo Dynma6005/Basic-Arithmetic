6d4d10fbbd960d5bf25256e96af66dec02a33c51
Dynma6005 <105459928+Dynma6005@users.noreply.github.com>
2022-06-24 02:36:20 +0100

    Add files via upload

---
+++ operation.js
@@ -0,0 +1,12 @@
+let x,y;
+x = parseFloat(parseInt(prompt("Enter the first number")));
+y = parseFloat(parseInt(prompt("Enter the second number")));
+let z = x+y;
+let a = x-y;
+let b = x*y;
+let c = x/y;
+
+alert("the sum of given numbers is:"+z);
+alert("the difference of given number is:"+a);
+alert("the product of given number is:"+b);
+alert("the quotient of given number is:"+c);
